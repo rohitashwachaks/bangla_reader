e4b66f192afb81f321a6b080788b78436aa76737
CaptainNemo <rohitashwachaks@gmail.com>
2022-11-19 03:46:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from common.config import WELCOME_MESSAGE
+import os
+
+from clients.image_ingestion_client import ImageReader_Client
 
 if __name__ == "__main__":
-    print(WELCOME_MESSAGE)
+    image_reader_client = ImageReader_Client()
+    image_reader_client.legend
+
+
+
