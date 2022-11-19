4bdf177e8027af1aaba680a332d5436d35de69a9
CaptainNemo <rohitashwachaks@gmail.com>
2022-11-19 03:46:31 -0600

    Update config.py

--- common/config.py
+++ common/config.py
@@ -1,3 +1,5 @@
 import os
 
 WELCOME_MESSAGE = os.getenv('WELCOME_MESSAGE', 'Hello World!')
+LOCAL_PATH = os.getenv('LOCAL_PATH', '/Users/rchaks/Code/GitHub/bangla_reader/data')
+LEGEND_FILENAME = os.getenv('LEGEND_FILENAME', 'legend.csv')
