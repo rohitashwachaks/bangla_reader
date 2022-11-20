a3a1582bbadbee50908dccc5e2926a0d49115703
CaptainNemo <rohitashwachaks@gmail.com>
2022-11-20 01:54:12 -0600

    Update image_ingestion_client.py

--- clients/image_ingestion_client.py
+++ clients/image_ingestion_client.py
@@ -1,29 +1,45 @@
+import io
+import json
 import os
-from common.config import LOCAL_PATH, LEGEND_FILENAME
-import pandas as pd
+
+from common.config import LOCAL_PATH, LEGEND_FILENAME, CONTAINER_NAME, STORAGE_ACCOUNT_NAME, STORAGE_ACCOUNT_KEY
+from azure.storage.blob import BlobServiceClient
 
 
-class ImageReader_Client:
-    def __init__(self) -> None:
-        self._image_root_path = os.path.join(LOCAL_PATH, 'img')
+class ImageReaderClient:
+    def __init__(self, local: bool = False) -> None:
+        self._local = local
+        self._legend = None
+        blob_service_client = BlobServiceClient(
+            account_url=f"https://{STORAGE_ACCOUNT_NAME}.blob.core.windows.net",
+            credential=STORAGE_ACCOUNT_KEY)
+        self._container_client = blob_service_client.get_container_client(container=CONTAINER_NAME)
 
         self.__load_legend__()
+
+        self.__walk_blobs__()
         return
 
-    def __load_legend__(self)->None:
-        legend_file = os.path.join(LOCAL_PATH, LEGEND_FILENAME)
-        print(legend_file)
-        self._legend = None
+    def __load_legend__(self) -> None:
         try:
-            self._legend = pd.read_csv(legend_file, header=None, names=['index', 'district_bn']).set_index('index').to_dict()
-            self._legend = self._legend['district_bn']
+            if self._local:
+                legend_filename = os.path.join(LOCAL_PATH, LEGEND_FILENAME)
+                with open(legend_filename, 'r') as fp:
+                    self._legend = json.load(fp)
+            else:
+                blob_client = self._container_client.get_blob_client(blob=LEGEND_FILENAME)
+                data_stream = blob_client.download_blob()
+                data_stream = data_stream.readall().decode()
+                self._legend = json.loads(data_stream)
         except Exception as ex:
             print(f'Failed to load legend file.\n {ex}')
 
+        return
+
+    def __walk_blobs__(self) -> None:
+        gen = self._container_client.walk_blobs()
+
+        pass
     @property
     def legend(self):
         return self._legend
-
-
-
-
