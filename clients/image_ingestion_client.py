8b8651f58cc1ad0f18491d6ef8d4bdbc872a4dc5
CaptainNemo <rohitashwachaks@gmail.com>
2022-12-04 00:20:50 -0600

    Update image_ingestion_client.py

--- clients/image_ingestion_client.py
+++ clients/image_ingestion_client.py
@@ -1,45 +1,63 @@
+import base64
 import io
 import json
 import os
+from typing import Union
 
-from common.config import LOCAL_PATH, LEGEND_FILENAME, CONTAINER_NAME, STORAGE_ACCOUNT_NAME, STORAGE_ACCOUNT_KEY
-from azure.storage.blob import BlobServiceClient
+from tqdm import tqdm
+
+from common.config import LOCAL_PATH, LOCAL_CONTENT_FILENAME, CONTAINER_NAME, STORAGE_ACCOUNT_NAME, STORAGE_ACCOUNT_KEY,\
+    LOCAL_CONTENT_FILENAME
+from azure.storage.blob import BlobServiceClient, BlobClient
 
 
 class ImageReaderClient:
-    def __init__(self, local: bool = False) -> None:
-        self._local = local
+    def __init__(self, local: bool = False, container_name: Union[None, str]= None) -> None:
         self._legend = None
+        self._content = {}
+        self._local = local
+
         blob_service_client = BlobServiceClient(
             account_url=f"https://{STORAGE_ACCOUNT_NAME}.blob.core.windows.net",
             credential=STORAGE_ACCOUNT_KEY)
-        self._container_client = blob_service_client.get_container_client(container=CONTAINER_NAME)
 
-        self.__load_legend__()
+        container_name = CONTAINER_NAME if container_name is None else container_name
+        self._container_client = blob_service_client.get_container_client(container=container_name)
 
-        self.__walk_blobs__()
-        return
+        local_content_file = os.path.join(LOCAL_PATH, CONTAINER_NAME, LOCAL_CONTENT_FILENAME)
+        if local:
+            if not os.path.exists(local_content_file):
+                local = False
+                print('Local content file not found. Going online')
+                local_content_folder = '/'.join(local_content_file.split('/')[:-1])
+                os.makedirs(local_content_folder, exist_ok=True)
+        self._local_content_file = local_content_file
 
-    def __load_legend__(self) -> None:
-        try:
-            if self._local:
-                legend_filename = os.path.join(LOCAL_PATH, LEGEND_FILENAME)
-                with open(legend_filename, 'r') as fp:
-                    self._legend = json.load(fp)
-            else:
-                blob_client = self._container_client.get_blob_client(blob=LEGEND_FILENAME)
-                data_stream = blob_client.download_blob()
-                data_stream = data_stream.readall().decode()
-                self._legend = json.loads(data_stream)
-        except Exception as ex:
-            print(f'Failed to load legend file.\n {ex}')
+        if local:
+            with open(local_content_file, 'r') as fp:
+                self._content = json.load(fp)
+
+        else:
+            self.__walk_blobs__()
 
         return
 
     def __walk_blobs__(self) -> None:
-        gen = self._container_client.walk_blobs()
+        gen = self._container_client.list_blobs()
+        for file in (pbar := tqdm(gen)):
+            pbar.set_description(file.name)
+            blob_client: BlobClient = self._container_client.get_blob_client(blob=file.name)
+            content = base64.b64encode(blob_client.download_blob().readall()).decode()
+            file_name = '.'.join(file.name.split('.')[:-1])
+            self._content[file_name] = {
+                'content': content
+            }
+        return
 
-        pass
     @property
-    def legend(self):
-        return self._legend
+    def content(self):
+        return self._content
+
+    @property
+    def content_file(self):
+        return self._local_content_file
