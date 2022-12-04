1f5c2c148916171920e690ec29a100c8734778f0
CaptainNemo <rohitashwachaks@gmail.com>
2022-12-04 00:22:13 -0600

    delete credentials

--- common/config.py
+++ common/config.py
@@ -5,8 +5,8 @@ LOCAL_PATH = os.getenv('LOCAL_PATH', '/Users/rchaks/Code/GitHub/bangla_reader/da
 LOCAL_CONTENT_FILENAME = os.getenv('LOCAL_CONTENT_FILENAME', 'legend.json')
 
 STORAGE_ACCOUNT_NAME = os.getenv('STORAGE_ACCOUNT_NAME', 'bangladocumentstore')
-STORAGE_ACCOUNT_KEY = os.getenv('STORAGE_ACCOUNT_KEY', '92xh0aGHCL8sFXMxqNiT+JTK2My29+UO3LS/f50pPW1cq651AUbk+55hhO0PXe66kU0HMABtZnrc+AStHrStGg==')
+STORAGE_ACCOUNT_KEY = os.getenv('STORAGE_ACCOUNT_KEY', '')
 CONTAINER_NAME = os.getenv('CONTAINER_NAME', 'book-1')
 DIRECTORY_PATH = os.getenv('DIRECTORY_PATH', 'img')
 
-GCP_VISION_KEY = os.getenv('GCP_VISION_KEY', 'AIzaSyCaTmXIpYUOGGex-_PsDR_63cJHzgv01m0')
+GCP_VISION_KEY = os.getenv('GCP_VISION_KEY', '')
