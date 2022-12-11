421d5673038f4ed7d79ffb9b336810f33919d10c
CaptainNemo <rohitashwachaks@gmail.com>
2022-12-11 03:56:32 -0600

    upload image

---
+++ src/clients/storage_client.py
@@ -0,0 +1,23 @@
+import io
+import json
+
+from google.cloud import storage
+from google.cloud.storage import Blob
+
+
+class StorageClient:
+    with open('/Users/rchaks/Code/GitHub/bangla_reader/src/common/augmented-tract-370404-bf2bc13a4ccf.json',
+              'r') as fp:
+        credentials_dict = json.load(fp)
+
+    __client = storage.Client.from_service_account_info(credentials_dict)
+    __bucket = __client.get_bucket('bangla-reader')
+
+    @staticmethod
+    def upload_blob(data, filename: str, ):
+        blob: Blob = StorageClient.__bucket.blob(filename)
+        with io.BytesIO() as fp:
+            fp.write(data)
+            fp.seek(0)
+            blob.upload_from_file(fp)
+        return
--- src/image_upload.py
+++ src/image_upload.py
@@ -1,6 +1,9 @@
 import base64
+import uuid
 
 from flask import Blueprint, request, render_template
+
+from src.clients.storage_client import StorageClient
 from src.clients.vision_client import VisionAPI
 
 image_upload = Blueprint('image_upload', __name__)
@@ -8,6 +11,14 @@ image_upload = Blueprint('image_upload', __name__)
 
 @image_upload.route('/upload/', methods=['POST'])
 def upload_image():
+    filename = str(uuid.uuid4())
+    StorageClient.upload_blob(data=request.data, filename=filename + '/img.jpeg')
     data = base64.b64encode(request.data).decode()
     decoded_text = VisionAPI.ocr_request(data)
-    return decoded_text['responses'][0]['fullTextAnnotation']['text']
+    decoded_text = decoded_text['responses'][0]['fullTextAnnotation']['text']
+    decoded_text = repr(decoded_text)[1:-1].replace('"', "'")#.split('।')
+    StorageClient.upload_blob(data=decoded_text.encode(), filename=filename+'/OCR.txt')
+    return {
+        'id': filename,
+        'text': decoded_text
+    }
